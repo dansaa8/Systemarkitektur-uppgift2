69fbff5131b5d051fea56e9b6d322072f0f86f7d
Dan Saa <dansaa8@protonmail.com>
2023-10-13 11:19:59 +0200

    Add extra formating for date.

--- src/main/java/com/example/systemarkitekturuppgift2/JacksonConfig.java
+++ src/main/java/com/example/systemarkitekturuppgift2/JacksonConfig.java
@@ -1,6 +1,7 @@
 package com.example.systemarkitekturuppgift2;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import jakarta.ws.rs.ext.ContextResolver;
 import jakarta.ws.rs.ext.Provider;
@@ -13,6 +14,7 @@ public class JacksonConfig implements ContextResolver<ObjectMapper> {
     public JacksonConfig() {
         objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
     }
 
     @Override
