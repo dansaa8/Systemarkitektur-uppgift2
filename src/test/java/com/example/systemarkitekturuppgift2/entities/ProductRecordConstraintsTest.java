d0da114ad6d692117889de335f169bd93fa52117
Dan Saa <dansaa8@protonmail.com>
2023-10-15 15:42:09 +0200

    Tests for rating constraint implemented.

--- src/test/java/com/example/systemarkitekturuppgift2/entities/ProductRecordConstraintsTest.java
+++ src/test/java/com/example/systemarkitekturuppgift2/entities/ProductRecordConstraintsTest.java
@@ -45,7 +45,7 @@ public class ProductRecordConstraintsTest {
                 Category.COMPUTERS, 4, LocalDate.now(), LocalDate.now());
         ProductRecord p2 = new ProductRecord(1, "Example Product",
                 Category.COMPUTERS, 4, LocalDate.now(), LocalDate.now());
-        List<ProductRecord> products = new ArrayList<>(Arrays.asList(new ProductRecord[]{p1, p2}));
+        List<ProductRecord> products = new ArrayList<>(Arrays.asList(p1, p2));
 
         Set<ConstraintViolation<List<ProductRecord>>> violations = validator.validate(products);
 
@@ -98,6 +98,39 @@ public class ProductRecordConstraintsTest {
         ProductRecord p = new ProductRecord(0, "Raspberry Pi",
                 Category.COMPUTERS, 4, LocalDate.now(), LocalDate.now());
         Set<ConstraintViolation<ProductRecord>> violations = validator.validate(p);
+
+        assertThat(violations.size()).isEqualTo(0);
+    }
+
+    @Test
+    public void testRatingConstraintWithNegativeValueExpectViolation() {
+        ProductRecord p = new ProductRecord(0, "Raspberry Pi",
+                Category.COMPUTERS, -1, LocalDate.now(), LocalDate.now());
+        Set<ConstraintViolation<ProductRecord>> violations = validator.validate(p);
+
+        assertThat(violations.size()).isEqualTo(1);
+        assertThat(violations.iterator().next().getMessage()).isEqualTo("Rating must not be lower than 0");
+    }
+
+    @Test
+    public void testRatingConstraintWithTooHighValueExpectViolation() {
+        ProductRecord p = new ProductRecord(0, "Raspberry Pi",
+                Category.COMPUTERS, 11, LocalDate.now(), LocalDate.now());
+        Set<ConstraintViolation<ProductRecord>> violations = validator.validate(p);
+
+        assertThat(violations.size()).isEqualTo(1);
+        assertThat(violations.iterator().next().getMessage()).isEqualTo("Rating must not be higher than 10");
+    }
+
+    @Test
+    public void testRatingConstraintWithOkValuesExpectNoViolation() {
+        ProductRecord p1 = new ProductRecord(0, "Example Product",
+                Category.COMPUTERS, 0, LocalDate.now(), LocalDate.now());
+        ProductRecord p2 = new ProductRecord(1, "Example Product",
+                Category.COMPUTERS, 10, LocalDate.now(), LocalDate.now());
+        List<ProductRecord> products = new ArrayList<>(Arrays.asList(p1, p2));
+
+        Set<ConstraintViolation<List<ProductRecord>>> violations = validator.validate(products);
 
         assertThat(violations.size()).isEqualTo(0);
     }
